89c42979d5ffeae0aff9eafdd89753ab3afffedf
LemonLime009 <135552380+LemonLime009@users.noreply.github.com>
2023-09-30 20:08:17 +0000

    sdkjfhjhsdf

--- src/clock.py
+++ src/clock.py
@@ -1,1 +1,19 @@
 import time
+
+class Clock():
+    def __init__(self, rate):
+        self.rate = rate
+        self.spf = 1 / rate
+        self.start = None
+        self.end = None
+        self.exe = None
+
+    def startTicker(self):
+        self.start = time.time()
+
+    def endTicker(self):
+        self.end = time.time()
+        self.exe = self.end - self.start
+
+        if self.exe <= self.spf:
+            time.sleep(self.spf - self.exe)
