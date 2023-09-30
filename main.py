7ada23c1a524a2bd7fdd1e864f1b61c004d89be5
LemonLime009 <135552380+LemonLime009@users.noreply.github.com>
2023-09-30 19:59:43 +0000

    Made changes

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.matrix import Matrix
+
+display = Matrix(20,20)
+
+display.clear(1)
+
+display.rect(0,0,4,7)
+display.point(19,19)
+
+display.update()
--- src/matrix.py
+++ src/matrix.py
@@ -1,3 +1,5 @@
+import os
+
 class Matrix:
     def __init__(self, width, height):
         self.width, self.height = width, height
@@ -8,9 +10,27 @@ class Matrix:
     def update(self):
         for row in self.grid:
             print(''.join(row))
+    
+    def clear(self, mode):
+        if mode == 1:
+            os.system("clear")
+        elif mode == 2:
+            for i in range(self.width):
+                for j in range(self.height):
+                    self.grid[i][j] = self.empty
+            os.system("clear")
+            self.update()
 
-    def dot(self, x, y):
+    def point(self, x, y):
         self.grid[x][y] = self.full
+
+    def line(self, x1, y1, x2, y2):
+        if x1 == x2 and y1 != y2:
+            for i in range(min(y1, y2), max(y1, y2) + 1):
+                self.grid[i][x1] = self.full
+        elif y1 == y2 and x1 != x2:
+            for i in range(min(x1, x2), max(x1, x2) + 1):
+                self.grid[y1][i] = self.full
 
     def rect(self, x, y, length, width):
         for i in range(x, x + length):
@@ -18,28 +38,23 @@ class Matrix:
                 self.grid[i][j] = self.full
 
     def circle(self, x, y, radius):
-    	cx = 0  # Initialize cx and cy to 0
-    	cy = radius
-    	decision = 1 - radius
+        cx = 0
+        cy = radius
+        decision = 1 - radius
 
-    	while cx <= cy:
-        	# Plot the eight points of the circle
-        	self.grid[cy + y][cx + x] = self.full
-        	self.grid[cy + x][cx + y] = self.full
-        	self.grid[cy - x][cx + y] = self.full
-        	self.grid[cy - y][cx + x] = self.full
-        	self.grid[cy - y][cx - x] = self.full
-        	self.grid[cy - x][cx - y] = self.full
-        	self.grid[cy + x][cx - y] = self.full
-        	self.grid[cy + y][cx - x] = self.full
+        while cx <= cy:
+            self.grid[cy + y][cx + x] = self.full
+            self.grid[cx + y][cy + x] = self.full
+            self.grid[-cx + y][cy + x] = self.full
+            self.grid[-cy + y][cx + x] = self.full
+            self.grid[-cy + y][-cx + x] = self.full
+            self.grid[-cx + y][-cy + x] = self.full
+            self.grid[cx + y][-cy + x] = self.full
+            self.grid[cy + y][-cx + x] = self.full
 
-        	cx += 1
-        	if decision <= 0:
-            	decision += 2 * cx + 1
-        	else:
-            	cy -= 1
-            	decision += 2 * (cx - cy) + 1
-
-matrix = Matrix(20, 20)
-matrix.circle(10, 10, 5)
-matrix.update()
+            cx += 1
+            if decision <= 0:
+                decision += 2 * cx + 1
+            else:
+                cy -= 1
+                decision += 2 * (cx - cy) + 1
