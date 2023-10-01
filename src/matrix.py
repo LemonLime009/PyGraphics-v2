f8f7c3a27b4308eb8991942d2421d8f020d5197e
LemonLime009 <135552380+LemonLime009@users.noreply.github.com>
2023-10-01 12:52:32 -0400

    Update matrix.py

--- src/matrix.py
+++ src/matrix.py
@@ -1,60 +1,66 @@
 import os
 
 class Matrix:
-    def __init__(self, width, height):
-        self.width, self.height = width, height
-        self.empty = "  "
-        self.full = " #"
-        self.grid = [[self.empty for _ in range(self.width)] for _ in range(self.height)]
+  def __init__(self, width, height):
+    self.width, self.height = width, height
+    self.empty = "  "
+    self.full = " #"
+    self.grid = [[self.empty for _ in range(self.width)] for _ in range(self.height)]
 
-    def update(self):
-        for row in self.grid:
-            print(''.join(row))
-    
-    def clear(self, mode):
-        if mode == 1:
-            os.system("clear")
-        elif mode == 2:
-            for i in range(self.width):
-                for j in range(self.height):
-                    self.grid[i][j] = self.empty
-            os.system("clear")
-            self.update()
+  def update(self):
+    for row in self.grid:
+      print(''.join(row))
 
-    def point(self, x, y):
-        self.grid[x][y] = self.full
+  def clear(self, mode):
+    if mode == 1:
+      os.system("clear")
+    elif mode == 2:
+      for i in range(self.width):
+        for j in range(self.height):
+          self.grid[i][j] = self.empty
+      os.system("clear")
+      self.update()
 
-    def line(self, x1, y1, x2, y2):
-        if x1 == x2 and y1 != y2:
-            for i in range(min(y1, y2), max(y1, y2) + 1):
-                self.grid[i][x1] = self.full
-        elif y1 == y2 and x1 != x2:
-            for i in range(min(x1, x2), max(x1, x2) + 1):
-                self.grid[y1][i] = self.full
+  def point(self, x, y):
+    self.grid[x][y] = self.full
 
-    def rect(self, x, y, length, width):
-        for i in range(x, x + length):
-            for j in range(y, y + width):
-                self.grid[i][j] = self.full
+  def line(self, x1, y1, x2, y2):
+    if x1 == x2 and y1 != y2:
+      for i in range(min(y1, y2), max(y1, y2) + 1):
+        self.grid[i][x1] = self.full
+    elif y1 == y2 and x1 != x2:
+      for i in range(min(x1, x2), max(x1, x2) + 1):
+        self.grid[y1][i] = self.full
 
-    def circle(self, x, y, radius):
-        cx = 0
-        cy = radius
-        decision = 1 - radius
+  def rect(self, x, y, length, width):
+    for i in range(x, x + length):
+      for j in range(y, y + width):
+        self.grid[i][j] = self.full
 
-        while cx <= cy:
-            self.grid[cy + y][cx + x] = self.full
-            self.grid[cx + y][cy + x] = self.full
-            self.grid[-cx + y][cy + x] = self.full
-            self.grid[-cy + y][cx + x] = self.full
-            self.grid[-cy + y][-cx + x] = self.full
-            self.grid[-cx + y][-cy + x] = self.full
-            self.grid[cx + y][-cy + x] = self.full
-            self.grid[cy + y][-cx + x] = self.full
+  def circle(self, x, y, radius):
+    cx = 0
+    cy = radius
+    decision = 1 - radius
 
-            cx += 1
-            if decision <= 0:
-                decision += 2 * cx + 1
-            else:
-                cy -= 1
-                decision += 2 * (cx - cy) + 1
+    while cx <= cy:
+      self.grid[cy + y][cx + x] = self.full
+      self.grid[cx + y][cy + x] = self.full
+      self.grid[-cx + y][cy + x] = self.full
+      self.grid[-cy + y][cx + x] = self.full
+      self.grid[-cy + y][-cx + x] = self.full
+      self.grid[-cx + y][-cy + x] = self.full
+      self.grid[cx + y][-cy + x] = self.full
+      self.grid[cy + y][-cx + x] = self.full
+
+      cx += 1
+      if decision <= 0:
+        decision += 2 * cx + 1
+      else:
+        cy -= 1
+        decision += 2 * (cx - cy) + 1
+
+  def prism(self, height, width, length, x, y, z):
+    for i in range(height):
+      for j in range(width):
+        for k in range(length):
+          self.grid[y + i][x + j + k] = self.full
